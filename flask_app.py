973ee965831915348f7c9c00a826537a15d6edce
Ruben <ruben_jongerden@hotmail.com>
2023-07-29 08:00:57 +0200

    Flask aangepast

--- flask_app.py
+++ flask_app.py
@@ -4,6 +4,6 @@ app=Flask (__name__)
 
 @app.route('/')
 def home():
-    return "<p>Hello world!<p>"
+    return "<p>Welkom in les 11, vraag 6<p>"
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
